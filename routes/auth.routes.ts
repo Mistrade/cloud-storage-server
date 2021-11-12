501f07f613405f8f831d54f8709beb896154d1bc
Андрей Черников <ac@x-lab.com.ru>
2021-11-12 13:34:53 +0500

    Добавлена логика авторизации, а именно:
    1. Добавлен новый роут - /login
    2. Внутри роута выполняются проверки на валидность email и password
    3. Происходит поиск юзера по email
    4. Сравниваются пароли в захешированном виде
    5. Устанавливаются cookie

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -3,14 +3,36 @@ import express, { Request, Response } from 'express'
 import bcrypt from 'bcryptjs'
 import { validateTools } from '../tools/validate'
 import validator from 'validator'
+import jwt from 'jsonwebtoken'
+import config from 'config'
 
 interface RegistrationInput {
   email: string,
   password: string
 }
 
+interface LoginResponse {
+  userData: UserDataModel
+}
+
+interface UserDataModel {
+  id: string,
+  email: string,
+  storageSpace: number,
+  usingSpace: number,
+  usingPercent: number,
+}
+
 interface ResponseModel {
   message: string
+}
+
+export interface Token {
+  id: string,
+  email: string,
+  date: number,
+  dateEnd: number,
+  type: 'access' | 'refresh'
 }
 
 const router = express.Router()
@@ -46,14 +68,89 @@ router.post( '/registration',
       return res.status(200).json({message: 'Поздравляем! Вы успешно зарегистрированы!'})
     } catch (e) {
       console.log( e )
-      res.status( 500 ).json( {
+      return res.status( 500 ).json( {
         message: 'Во время обработки регистрации нового пользователя на сервере произошла ошибка',
       } )
     }
   } )
 
-router.post('/login', async () => {
+router.post('/login', async (req: Request<RegistrationInput>, res: Response<LoginResponse | ResponseModel>) => {
+  try {
+    const {email, password} = req.body
 
+    if(!validator.isEmail(email)){
+      return res.status(400).json({message: 'Указан невалидный Email-адрес'})
+    }
+
+    const passwordValidate = validateTools.checkPassword(password)
+
+    if(!passwordValidate.status){
+      return res.status(400).json({message: 'Указан некорректный пароль. Пароль должен содержать не менее 8 и не более 32 символов'})
+    }
+
+    const user: UserModel | null = await User.findOne({email})
+
+    if(!user){
+      return res.status(404).json({message: 'Пользователь с таким Email не найден!'})
+    }
+
+    const passwordEquality = await bcrypt.compare(password, user.password)
+
+    if(!passwordEquality){
+      return res.status(403).json({message: 'Указаны неверный логин или пароль!'})
+    }
+
+    const tokenSettings: Token = {
+      id: user.id,
+      email: user.email,
+      date: Date.now(),
+      dateEnd: Date.now() + 1000*60*15,
+      type: 'access'
+    }
+
+    const token = jwt.sign(tokenSettings, config.get('secretKey'), {
+      expiresIn: '15m'
+    })
+
+    const refreshTokenSettings: Token = {
+      id: user.id,
+      email: user.email,
+      date: Date.now(),
+      dateEnd: Date.now() + 1000*60*60*24,
+      type: 'refresh',
+    }
+
+    const refreshToken = jwt.sign(refreshTokenSettings, config.get('secretKey'), {
+      expiresIn: '1d'
+    })
+
+    res.cookie('AccessToken', token, {
+      maxAge: 60*15,
+      path: '/',
+      httpOnly: true,
+    } )
+
+    res.cookie('RefreshToken', refreshToken, {
+      maxAge: 60*60*24,
+      path: '/',
+      httpOnly: true
+    })
+
+    return res.status(200).json({
+      userData: {
+        email: user.email,
+        storageSpace: user.storageSpace,
+        usingSpace: user.usingSpace,
+        usingPercent: (user.usingSpace / user.storageSpace)*100,
+        id: user.id,
+      }
+    })
+  } catch (e) {
+    console.log( e )
+    return res.status( 500 ).json( {
+      message: 'Во время обработки регистрации нового пользователя на сервере произошла ошибка',
+    } )
+  }
 })
 
 
